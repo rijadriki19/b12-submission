fcaaf28c0941327d3384507f4d095b8e8f67336e
Rijad Foco <rijad.foco@hotmail.com>
2026-03-09 13:58:58 +0100

    fix: added env var for github run id

--- submit.py
+++ submit.py
@@ -1,3 +1,4 @@
+import os
 import json
 import hmac
 import hashlib
@@ -7,13 +8,18 @@ from datetime import datetime, timezone
 URL = "https://b12.io/apply/submission"
 SIGNING_SECRET = b"hello-there-from-b12"
 
+run_id = os.environ.get("GITHUB_RUN_ID")
+if not run_id:
+    raise RuntimeError("GITHUB_RUN_ID environment variable not found")
+
+# Build the payload
 payload = {
     "timestamp": datetime.now(timezone.utc).isoformat(timespec="milliseconds").replace("+00:00", "Z"),
     "name": "Rijad Foco",
     "email": "rijad.foco@hotmail.com",
-    "resume_link": "Na",
+    "resume_link": "Na", 
     "repository_link": "https://github.com/rijadriki19/b12-submission",
-    "action_run_link": "https://github.com/yourusername/yourrepo/actions/runs/${{ github.run_id }}"
+    "action_run_link": f"https://github.com/rijadriki19/b12-submission/actions/runs/{run_id}"
 }
 
 body = json.dumps(payload, sort_keys=True, separators=(",", ":")).encode("utf-8")
